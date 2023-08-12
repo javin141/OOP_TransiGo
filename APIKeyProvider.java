faac411ec3ad7d74f01bec742a28a56d98a4fde4
javin141 <66424374+javin141@users.noreply.github.com>
2023-08-13 03:22:08 +0800

    Create APIKeyProvider.java

---
+++ APIKeyProvider.java
@@ -0,0 +1,11 @@
+public interface APIKeyProvider {
+    String getAPIKey();
+}
+
+
+class API implements APIKeyProvider{
+	public String getAPIKey() {
+        String apiKey = "x2KGyywxRlCvM6OIfM19UQ==";
+		return apiKey;
+	}
+}
