63a75b347f465540a015528d4b8d3689ebbdd111
javin141 <javinjj141@gmail.com>
2023-08-15 13:18:37 +0800

    fixed naming scheme

--- application/HomepgController.java
+++ application/HomepgController.java
@@ -51,7 +51,7 @@ public class HomepgController {
 	}
 	
 	public void switchToCalculatorfare(ActionEvent event) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("busfare.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("BusFare.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
