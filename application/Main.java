1fd9c0450a653e1b9f55372cc9bd98210498e09f
javin141 <javinjj141@gmail.com>
2023-08-15 12:07:55 +0800

    removed unnecessary

--- application/Main.java
+++ application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
             Parent root = FXMLLoader.load(getClass().getResource("Homepg.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
-            stage.setTitle("Bus Timings App");
+            stage.setTitle("Javin(P2224222), Roshan(2223458), Jonathan(2223627)");
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
